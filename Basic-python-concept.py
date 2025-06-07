9382495f6007b4500fe86f74a53197e06fa888d8
amitkumar755100 <adeshkumar741186@gmail.com>
2025-06-07 23:18:53 +0530

    Add new file

--- Basic-python-concept.py
+++ Basic-python-concept.py
@@ -1,39 +1,39 @@
 # celsius_to_fahrenheit
 
-# c=float(input("Enter tempreture in Celsius:-"))
-# # c=float(c)
-# f=(c * 9/5) + 32
-# # print('Fahrenheit:',f)
+c=float(input("Enter tempreture in Celsius:-"))
+# c=float(c)
+f=(c * 9/5) + 32
+# print('Fahrenheit:',f)
 
 
 
 # fahrenheit to celsius
 
-# f = float(input("farenheit:-"))
-# c = (f - 32) * 5/9
-# print("Celsius",c)
+f = float(input("farenheit:-"))
+c = (f - 32) * 5/9
+print("Celsius",c)
 
 
 # SI Calculator
 
-# P = float(input("Enter Principal amount (₹):"))
-# R = float(input("Enter Rate of Interest (%):"))
-# T = float(input("Enter Time (in years):"))
-# si= P*R*T/100
-# print("Simple Interest is:", si)
+P = float(input("Enter Principal amount (₹):"))
+R = float(input("Enter Rate of Interest (%):"))
+T = float(input("Enter Time (in years):"))
+si= P*R*T/100
+print("Simple Interest is:", si)
 
 
 # ASSIGNMENT 1: Basic Python Concepts
 
-# n1=int(input("Enter the first number:"))
-# n2=int(input("Enter the Second number:"))
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the Second number:"))
 
-# add = n1+n2
-# sub = n1-n2
-# multi = n1*n2
-# div = n1/n2
+add = n1+n2
+sub = n1-n2
+multi = n1*n2
+div = n1/n2
 
-# print("Addition:",add, "\nSubtract:", sub , "\nMultiplication", multi, "\ndivision", div)
+print("Addition:",add, "\nSubtract:", sub , "\nMultiplication", multi, "\ndivision", div)
 
  
 
